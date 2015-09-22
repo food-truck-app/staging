062b056981286af9968028396dce393dde2f69da
clareluna <clarelunamonahan@gmail.com>
2015-09-22 11:05:40 -0700

    changes made to server to avoid merge conflict

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 
-var userRouter = require(__dirname + './routes/user');
-var authRouter = require(__dirname + './routes/auth');
-
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/food_truck_app');
 
 app.use(bodyParser.urlencoded({
@@ -14,6 +11,11 @@ app.use(bodyParser.urlencoded({
 })); //allows objects and arrays to be encoded in a JSON like package
 
 app.use(passport.initialize()); //requires the passport package
+
+var userRouter = require(__dirname + './routes/user');
+var authRouter = require(__dirname + './routes/auth');
+var trucksRouter = require(__dirname + '/routes/trucks_routes');
+app.use('/api', trucksRouter);
 
 var router = express.Router();
 
@@ -23,10 +25,6 @@ router.route('/users')
 
 router.route('/authenticate')
 	.post(userRouter.authenticateUser);
-		
-var trucksRouter = require(__dirname + '/routes/routes');
-app.use('/api', trucksRouter);
-
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {

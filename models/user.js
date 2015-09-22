5772f82fb7f0ee42ab171872d086dcaefc20387c
clareluna <clarelunamonahan@gmail.com>
2015-09-22 10:46:02 -0700

    lambda based auth

--- models/user.js
+++ models/user.js
@@ -2,28 +2,40 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var eat = require('eat');
 
+//Lambda example
 var userSchema = new mongoose.Schema({
-	username: String,
-	basic: {
-		username: String,
-		password: String
-	}
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
 });
 
-userSchema.methods.generateHash = function(password, callback) {
-	bcrypt.hash(password, 8, function(err, hash) {
+userSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) return next();
+
+	bcrypt.genSalt(8, function(err, salt) {
+		if (err) return next(err);
+		
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+			if(err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+userSchema.methods.verifyPassword = function(password, callback) {
+	bcrypt.compare(password, this.password, function(err, isMatch) {
 		if(err) return callback(err);
-		this.basic.password = hash;
-		callback(null, hash);
-	}.bind(this));
-};
-
-userSchema.methods.createToken = function(password, callback) {
-	bcrypt.compare(password, this.basic.password, callback);
-};
-
-userSchema.methods.generateToken = function(callback) {
-	eat.encode({id: this._id}, process.env.APP_SECRET, callback);
+		callback(null, isMatch);
+	});
 };
 
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+var userRouter = require(__dirname + './routes/user');
+var authRouter = require(__dirname + './routes/auth');
+
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/food_truck_app');
 
+app.use(bodyParser.urlencoded({
+	extended: true
+})); //allows objects and arrays to be encoded in a JSON like package
+
+app.use(passport.initialize()); //requires the passport package
+
+var router = express.Router();
+
+router.route('/users')
+	.get(authRouter.isAuthenticated, userRouter.getUsers)
+	.post(userRouter.postUsers);
+
+router.route('/authenticate')
+	.post(userRouter.authenticateUser);
+		
 var trucksRouter = require(__dirname + '/routes/routes');
 app.use('/api', trucksRouter);
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {

63afbbcf5a2ebbc228e08844355c1712b4de58e7
Lady Blue <bluelady.2015@yahoo.com>
2015-09-23 01:49:56 -0700

    menu and cuisine data for foodtrucks

---
+++ database.js
@@ -0,0 +1,28 @@
+{"food trucks:"[
+  {"name": "Djung On Wheels", 
+    "cuisine": "Asian",
+    "menu": [
+      {"dish": "Orange Chicken"},
+      {"dish": "Red Curry Chicken"},
+      {"dish": "Pad Thai"},
+      {"dish": "Pad Kee Mao"},
+      {"dish": "Pad See Ew"},
+    ]
+  },
+  {"name": "Tacos El Tajin",
+    "cuisine": "Hispanic",
+    "menu": [
+      {"dish": "Regular Burrito"},
+      {"dish": "Super Burrito"}, 
+      {"dish": "Three Tacos"}
+    ]
+  },
+  {"name": "Hungry Me",
+    "cuisine": "Asian",
+    "menu": [
+      {"dish": "Japanese Curry"},
+      {"dish": "Beef Stew"},
+      {"dish": "Panang Curry"}
+    ]
+  }
+]}

9c20edea381ebb72d54b954a2d8089ff20ffe0ae
Emmett Agnew <eragnew@gmail.com>
2015-09-23 10:40:52 -0700

    Started truck tests

--- routes/trucks_routes.js
+++ routes/trucks_routes.js
@@ -3,6 +3,7 @@ var Truck = require(__dirname + '/../models/truck');
 var express = require('express');
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
+var eatAuth = require(__dirname + '/../lib/eat_auth');
 
 var trucksRoute = module.exports = exports = express.Router();
 
@@ -37,7 +38,7 @@ trucksRoute.get('/trucks/:day/:lng/:lat', function(req, res) {
   });
 });
 
-trucksRoute.post('/trucks', jsonParser, function(req, res) {
+trucksRoute.post('/trucks', jsonParser, eatAuth, function(req, res) {
   var newTruck = new Truck(req.body);
   newTruck.save(function(err, data) {
     if (err) return handleError(err, res);
@@ -45,7 +46,7 @@ trucksRoute.post('/trucks', jsonParser, function(req, res) {
   });
 });
 
-trucksRoute.put('/trucks/:id', jsonParser, function(req, res) {
+trucksRoute.put('/trucks/:id', jsonParser, eatAuth, function(req, res) {
   var newTruckBody = req.body;
   Truck.update({_id: req.params.id}, newTruckBody, function(err, data) {
     if (err) return handleError(err, res);
@@ -53,7 +54,7 @@ trucksRoute.put('/trucks/:id', jsonParser, function(req, res) {
   });
 });
 
-trucksRoute.delete('/trucks/:id', jsonParser, function(req, res) {
+trucksRoute.delete('/trucks/:id', jsonParser, eatAuth, function(req, res) {
   Truck.remove({_id: req.params.id}, function(err) {
     if (err) return handleError(err, res);
     res.json({msg: 'success'});
---
+++ test/truck_test.js
@@ -0,0 +1,57 @@
+var chai = require('chai');
+var chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+var expect = chai.expect;
+process.env.MONGO_URL = 'mongodb://localhost/food_truck_app_test';
+require(__dirname + '/../server');
+var mongoose = require('mongoose');
+var Truck = require(__dirname + '/../models/truck');
+var User  = require(__dirname + '/../models/user');
+
+var token = '';
+var server_url = 'localhost:3000/api';
+
+describe('the truck resource', function() {
+  before(function(done) {
+    chai.request(server_url)
+      .post('/signup')
+      .send({username: 'test', password: 'foobar123'})
+      .end(function(err, res) {
+        token = res.body.token;
+        done();
+      });
+  });
+
+  after(function(done) {
+    mongoose.connection.db.dropDatabase(function(err) {
+      if (err) throw err;
+      done();
+    });
+  });
+
+  it('should be able to post truck documents', function(done) {
+    chai.request(server_url)
+      .post('/trucks')
+      .send({
+        truckname: 'Awesome Food Truck',
+        cuisine: 'Indian',
+        description: 'An awesome food truck!',
+        menu: [{item: 'Tikka Masala'}, {item: 'Naan'}],
+        locations: {
+          monday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+          tuesday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+          wednesday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+          thursday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+          friday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+          saturday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+          sunday: {name: 'SLU', loc: [-122.335827, 47.623553]},
+        },
+        token: token
+      }).
+      end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});

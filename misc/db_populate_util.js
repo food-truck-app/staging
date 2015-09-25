b6de3e3ebcea3be752edfd23d9c9a83adbe9a438
Emmett Agnew <eragnew@gmail.com>
2015-09-25 10:17:45 -0700

    Added screenshot

--- misc/db_populate_util.js
+++ misc/db_populate_util.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/food_truck_app');
+// mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/food_truck_app');
+// mongoose.connect('mongodb://heroku_prt3tf2f:535sectp8nnkl10lbha2eric16@ds051953.mongolab.com:51953/heroku_prt3tf2f');
+mongoose.connect('mongodb://heroku_4tdjdwk7:uvnenf6jv8n9n2er3s1jjrk80d@ds051883.mongolab.com:51883/heroku_4tdjdwk7');
 
 var Truck = require(__dirname + '/../models/truck');
 var fs = require('fs');
@@ -21,4 +23,6 @@ fs.readFile(filename, function(err, data) {
       console.log(data);
     });
   }
+
+  // mongoose.disconnect();
 });

c5f0b2132e110b97ff9c896c67db683c4f1ffefc
Clare Monahan <clareluna@users.noreply.github.com>
2015-09-25 00:09:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://<dbuser>:<dbpassword>@ds051883.mongolab.com:51883/heroku_4tdjdwk7');
-process.env.APP_SECRET = process.env.APP_SECRET || 'forrealchangemechangeme';
+mongoose.connect(process.env.MONGO_URL || 'mongodb://heroku_prt3tf2f:535sectp8nnkl10lbha2eric16@ds051953.mongolab.com:51953/heroku_prt3tf2f');
+process.env.APP_SECRET = process.env.APP_SECRET || 'b59bd6650d5bf1f14f38ad428a175c197802fde7f1be4f578d01b75a4f47d6f4d3d0d5e88fba2da40d68fb56041764f1';
 
 var trucksRouter = require(__dirname + '/routes/trucks_routes');
 var usersRouter = require(__dirname + '/routes/users_routes');

559738480c9eec4efbe0409759cb439a537494f7
clareluna <clarelunamonahan@gmail.com>
2015-09-24 22:35:57 -0700

    not right

--- js/map.js
+++ js/map.js
@@ -33,13 +33,15 @@ $(document).ready(function() {
       for (var i = 0; i < data.length; i++) {
         var truck = data[i];
 //        locations.push([truck.truckname || truck.locations[day].name, truck.locations[day].loc[1], truck.locations[day].loc[0], i]);
+
           locations.push
           (
               {
                   name: truck.truckname || truck.locations[day].name,
                   lat: truck.locations[day].loc[1],
                   lon: truck.locations[day].loc[0],
-                  cuisine: truck.cuisine
+                  cuisine: truck.cuisine,
+                  days: truck.locations
               }
           );
       }
@@ -49,14 +51,16 @@ $(document).ready(function() {
 
       for (i = 0; i < locations.length; i++) {
         var location = locations[i];
-        console.log(location);
+        
         marker = new google.maps.Marker({
           position: new google.maps.LatLng(location.lat, location.lon),
           map: map,
           title: location.name
         });
         marker.cuisine = location.cuisine;
-        console.log(marker.cuisine);
+        marker.days = location.days;
+        marker.truck = location.name;
+
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
           return function() {
             console.log(marker);
@@ -75,85 +79,98 @@ $(document).ready(function() {
     });
   }
 
-  function onChangeCuisine (cuisineType) {
-    if (markerCollection.length === 1) {
-      clearMarkers();
-      $('#menuRow').remove();
-      $.ajax({
-        url: "/api/trucks",
-        dataType: "json"
-        }).success(function (data) {
-        var locations = [];
-        var date = new Date();
-        var dateDay = date.getDay();
-        var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var day = dayArray[dateDay].toLowerCase();
-
-        for (var i = 0; i < data.length; i++) {
-          var truck = data[i];
-  //        locations.push([truck.truckname || truck.locations[day].name, truck.locations[day].loc[1], truck.locations[day].loc[0], i]);
-            locations.push
-            (
-                {
-                    name: truck.truckname || truck.locations[day].name,
-                    lat: truck.locations[day].loc[1],
-                    lon: truck.locations[day].loc[0],
-                    cuisine: truck.cuisine
-                }
-            );
-        }
-
-        var infowindow = new google.maps.InfoWindow();
-        var marker, i;
-
-        for (i = 0; i < locations.length; i++) {
-          var location = locations[i];
-          console.log(location);
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(location.lat, location.lon),
-            map: map,
-            title: location.name
-          });
-          marker.cuisine = location.cuisine;
-          console.log(marker.cuisine);
-          google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-              console.log(marker);
-              infowindow.setContent(marker.title);
-              infowindow.open(map, marker);
-              var truckName = data[i].locations.monday.name;
-              var menuItems = data[i].menu.map(function(item) {
-                return item.item;
-              });
-              var menuList = menuItems.join(', ');
-              appendMenu(truckName, menuList);
-            }
-          })(marker, i));
-          markerCollection.push(marker);
-        }
-        for (var i = 0; i < markerCollection.length; i++) {
-          var mapMarker = markerCollection[i];
-          if (mapMarker.cuisine === cuisineType) {
-            mapMarker.setVisible(true);
-          }
-          else {
-            mapMarker.setVisible(false);
-          };
-        }
-      });
-    } else {
-      for (var i = 0; i < markerCollection.length; i++) {
-        $('#menuRow').remove();
-        var mapMarker = markerCollection[i];
-        if (mapMarker.cuisine === cuisineType) {
-          mapMarker.setVisible(true);
-        }
-        else {
-          mapMarker.setVisible(false);
-        };
+      function onChangeCuisine (cuisineType) {
+    for (var i = 0; i < markerCollection.length; i++) {
+      var mapMarker = markerCollection[i];
+      if (mapMarker.cuisine === cuisineType) {
+        mapMarker.setVisible(true);
       }
+      else {
+        $('#menuRow').remove();
+        mapMarker.setVisible(false);
+      };
     }
   }
+
+  // function onChangeCuisine (cuisineType) {
+  //   if (markerCollection.length === 1) {
+  //     clearMarkers();
+  //     $('#menuRow').remove();
+  //     $.ajax({
+  //       url: "/api/trucks",
+  //       dataType: "json"
+  //       }).success(function (data) {
+  //       var locations = [];
+  //       var date = new Date();
+  //       var dateDay = date.getDay();
+  //       var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  //       var day = dayArray[dateDay].toLowerCase();
+
+  //       for (var i = 0; i < data.length; i++) {
+  //         var truck = data[i];
+  // //        locations.push([truck.truckname || truck.locations[day].name, truck.locations[day].loc[1], truck.locations[day].loc[0], i]);
+  //           locations.push
+  //           (
+  //               {
+  //                   name: truck.truckname || truck.locations[day].name,
+  //                   lat: truck.locations[day].loc[1],
+  //                   lon: truck.locations[day].loc[0],
+  //                   cuisine: truck.cuisine
+  //               }
+  //           );
+  //       }
+
+  //       var infowindow = new google.maps.InfoWindow();
+  //       var marker, i;
+
+  //       for (i = 0; i < locations.length; i++) {
+  //         var location = locations[i];
+  //         console.log(location);
+  //         marker = new google.maps.Marker({
+  //           position: new google.maps.LatLng(location.lat, location.lon),
+  //           map: map,
+  //           title: location.name
+  //         });
+  //         marker.cuisine = location.cuisine;
+  //         console.log(marker.cuisine);
+  //         google.maps.event.addListener(marker, 'click', (function(marker, i) {
+  //           return function() {
+  //             console.log(marker);
+  //             infowindow.setContent(marker.title);
+  //             infowindow.open(map, marker);
+  //             var truckName = data[i].locations.monday.name;
+  //             var menuItems = data[i].menu.map(function(item) {
+  //               return item.item;
+  //             });
+  //             var menuList = menuItems.join(', ');
+  //             appendMenu(truckName, menuList);
+  //           }
+  //         })(marker, i));
+  //         markerCollection.push(marker);
+  //       }
+  //       for (var i = 0; i < markerCollection.length; i++) {
+  //         var mapMarker = markerCollection[i];
+  //         if (mapMarker.cuisine === cuisineType) {
+  //           mapMarker.setVisible(true);
+  //         }
+  //         else {
+  //           mapMarker.setVisible(false);
+  //         };
+  //       }
+  //     });
+  //   } else {
+  //     for (var i = 0; i < markerCollection.length; i++) {
+  //       $('#menuRow').remove();
+  //       var mapMarker = markerCollection[i];
+  //       if (mapMarker.cuisine === cuisineType) {
+  //         mapMarker.setVisible(true);
+  //       }
+  //       else {
+  //         mapMarker.setVisible(false);
+  //       };
+  //     }
+  //   }
+  // }
 
   $('.cuisine-dropdown')
     .on('click', 'a', function (event) {
